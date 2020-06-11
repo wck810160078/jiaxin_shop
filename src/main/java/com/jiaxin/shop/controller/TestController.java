7c5a5f5732d492bbab74a09bc0e5ac7cc3d639e7
wck <810160078@qq.com>
2020-06-11 12:00:25 +0800

    修改权限

--- src/main/java/com/jiaxin/shop/controller/TestController.java
+++ src/main/java/com/jiaxin/shop/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.jiaxin.shop.controller;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class TestController {
+    @Resource
+    RedisTemplate redisTemplate;
 
     @GetMapping("/test")
     public String test(){
+        redisTemplate.opsForValue().set("a","1");
+        String a = redisTemplate.opsForValue().get("a").toString();
+        System.out.println(a);
         return "正在开发中，敬请期待";
     }
+
 
 
 
